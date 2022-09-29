71ed8a464b9dd80abf4f5543f73a96bfab3fee07
irinooky <32641154+irinooky@users.noreply.github.com>
2022-09-29 10:42:26 +0200

    changed the formatDate function and added another apiKey

--- src/javaApp.js
+++ src/javaApp.js
@@ -49,8 +49,8 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "c62ebe2801487103359e5c2553337660";
-  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "a43564c91a6c605aeb564c9ed02e3858";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
