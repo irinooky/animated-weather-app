587531792248ec1a7bec98e29711445fd037d1b5
irinooky <32641154+irinooky@users.noreply.github.com>
2022-09-29 18:19:19 +0200

    removed 1 day

--- src/javaApp.js
+++ src/javaApp.js
@@ -28,7 +28,7 @@ function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
